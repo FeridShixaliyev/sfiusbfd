2fb194eeb9240d41af7f62082b4ccb698862930d
FeridShixaliyev <tu7b9ed4n@code.edu.az>
2022-03-05 10:26:27 +0400

    completed task1

---
+++ task.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr={ 1,3,4,12,14,18,43,32,25};
+            int i = 0;
+            int total = 0;
+            for (i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 3 == 0)
+                {
+                    total += arr[i];
+                }
+
+            }
+            Console.WriteLine(total);
+        }
+    }
+}
---
+++ task3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bir eded daxil edin: ");
+            double number = Convert.ToInt32(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                while (number > 1)
+                {
+                    number /= 2;
+                }
+                if (number == 1)
+                {
+                    Console.WriteLine("quvvetidir");
+                }
+                else if (number != 1)
+                {
+                    Console.WriteLine("quvveti deyil");
+                }
+            }
+            else if (number % 2 != 0)
+            {
+                Console.WriteLine("cut eded daxil edin:");
+                
+            }
+        }
+    }
+}
