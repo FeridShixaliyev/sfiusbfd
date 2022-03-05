a33e427ce8ab5bf4dcb2676af23069d1b7a81750
FeridShixaliyev <99806164+FeridShixaliyev@users.noreply.github.com>
2022-03-05 11:49:06 +0400

    completed task

---
+++ task2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace character_searching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char sr;    //bu daxil etdiyimiz herfdir
+            string str;     //bu daxil etdiyimiz sozdur
+            int i, count = 0, flag = 0;
+            Console.WriteLine("cumle veya soz daxil edin:");
+            str = Console.ReadLine();
+
+            Console.WriteLine("axtardiginiz herfi daxil edin :");
+            sr = char.Parse(Console.ReadLine());
+
+            for (i = 0; i < str.Length; i++)
+            {
+                if (str[i] == sr)
+                {
+                    flag = 1;
+
+                    count++;
+                }
+            }
+            if (flag == 1)
+                Console.WriteLine("isdifade " +  " olunub "+":" + count + " defe ");
+            else
+                Console.WriteLine("eded tapilmadi");
+        }
+    }
+}
